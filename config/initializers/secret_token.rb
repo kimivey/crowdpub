6518f2e85df0c6298a1a1a725f0484785d2e362e
Kim Iverson <kimpob@me.com>
2012-09-23 14:30:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdpub::Application.config.secret_token = '23abc844baf840a9a804c0f1214fba0a36e33db1b062d21e8d52f576c6e67510d51e753bbdae6e269117a77add5b60027b975e6f60bbdc42f640d1a7971099f6'
