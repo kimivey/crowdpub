50699afdf8803d6b5ff4804d89267ce7a583ea68
Kim Iverson <kimpob@me.com>
2012-09-24 11:18:54 -0700

    Fill in layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,61 +2,36 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  subject { page }
+
   describe "Home page" do
-    it "should have the h1 'CrowdPub'" do
-      visit root_path
-      page.should have_selector('h1', :text => 'CrowdPub')
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',
-        :text => "CrowdPub for Writers")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector 'h1', text: 'CrowdPub' }
+    it { should have_selector 'title', text: full_title('') }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-        :text => "CrowdPub for Writers | Help")
-    end
+    it { should have_selector 'h1', text: 'Help' }
+    it { should have_selector 'title', text: full_title('Help') }
+
   end
 
   describe "About page" do
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_selector('title',
-        :text => "CrowdPub for Writers | About Us")
-    end
+    it { should have_selector 'h1', text: 'About Us' }
+    it { should have_selector 'title', text: full_title('About Us') }
+
   end
 
   describe "Contact page" do
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact')
-    end
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-        :text => "CrowdPub for Writers | Contact")
-    end
+    it { should have_selector 'h1', text: 'Contact' }
+    it { should have_selector 'title', text: full_title('Contact') }
   end
-
 end
